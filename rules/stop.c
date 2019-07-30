baccd60f0bd72f002c18cff2b3852bf63426c71c
bkarjoo <bkarjoo@gmail.com>
2019-07-30 10:57:22 -0400

    modified build to load four files, modified file_reader to not load the variable, variables are loaded in build function, modified build to backup each new file

---
+++ rules/stop.c
@@ -0,0 +1,23 @@
+#include "../../etc/functions.h"
+stop_order_type = PRIMUS_STOP
+stop_order_side = SELL
+stop_size = 100
+stop_price =
+{
+
+}
+enable_trailing = TRUE
+trail_after_entry_complete = TRUE
+trail_once = FALSE
+trail_trigger =
+{
+
+}
+trail_how =
+{
+
+}
+trail_increment =
+{
+
+}
---
+++ rules/target.c
@@ -0,0 +1,29 @@
+#include "../../etc/functions.h"
+target_order_type = PRIMUS_AEL
+target_order_side = SELL
+target_destination = CSFB
+target_size = 100
+target_limit =
+{
+
+}
+ael_on_last = TRUE
+ael_on_second = FALSE
+ael_on_bid_ask = FALSE
+ael_convert_to_stop = FALSE
+ael_trigger =
+{
+
+}
+ael_price =
+{
+
+}
+ael_time_increment =
+{
+
+}
+ael_price_increment =
+{
+
+}
